b294cab1f53147a22a00d0161de03c4b8a2ba262
CoadingClass <govindsdr8055@gmail.com>
2024-07-26 17:57:35 +0530

    connected to dataBase

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log(`DB is Connected Successfully `);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.listen(process.env.PORT, () => {
+  console.log(`⚙︎ Server is running on http://localhost:${process.env.PORT}`);
+});
