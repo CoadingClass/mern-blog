8bcfd636024acc4bd9be5958bf1671fdcbb8cecd
CoadingClass <govindsdr8055@gmail.com>
2024-07-27 15:03:11 +0530

    Added Funcality og signUp page s

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 const app = express();
 
 dotenv.config();
@@ -12,6 +14,21 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use(express.json());
 app.listen(process.env.PORT, () => {
   console.log(`⚙︎ Server is running on http://localhost:${process.env.PORT}`);
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Server Error ";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
